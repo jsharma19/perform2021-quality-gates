f028b0bc01b4cf390924fade15c442fd55c8e8dd
Diego Quispe Cuenca <drqc0491@gmail.com>
2021-01-20 17:44:43 -0500

    pre-build script implemented

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,8 @@
 source_repo="https://github.com/diegorqc/perform2021-quality-gates"
-repo_folder="perform2021-quality-gates" 
 clone_folder="bootstrap"
-home_folder="/home/$shell_user"
+home_folder="/home/$USER"
 
+rm -rf bootstrap
 ##############################
 # Clone repo                 #
 ##############################
@@ -10,7 +10,8 @@ cd $home_folder
 mkdir "$clone_folder"
 cd "$home_folder/$clone_folder"
 git clone "$source_repo" .
-chown -R $shell_user $home_folder/$clone_folder
-cd $repo_folder
+chown -R $USER $home_folder/$clone_folder
+cd "$home_folder/$clone_folder/"
+chmod u+x ./build.sh  
 ./build.sh "$home_folder" "$clone_folder" "$source_repo"
 
